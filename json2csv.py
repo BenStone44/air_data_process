3d03ee425d6fef48108978b976cf3d230e2ffb40
陈时富 <shifu.chen@outlook.com>
2022-08-17 14:50:13 +0800

    s

--- json2csv.py
+++ json2csv.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+import random
+import numpy as np
 
 with open('air-data.json', encoding='utf-8') as f:
     line = f.readline()
@@ -12,6 +14,7 @@ data_step = []
 for i in range(179):
     keys.append(i + 1)
 
+index = 0
 for key in d:
     step = 0  # 记录步数
     step_time = 48  # 以48小时为步长
@@ -19,13 +22,23 @@ for key in d:
         list_day = []
         for i in range(178):
             list_day.append(d[key][step * step_time + i])
-        list_day.append(int(key))
+        list_day.append(index)
         data_step.append(list_day)
         step = step + 1
+    index = index + 1
 
+for key in d:
+    step = 0
+    start_point = 0
+    for i in range(47):
+
+
+
+
+
+np.random.shuffle(data_step)
 
 data_step = pd.DataFrame(columns=keys, data=data_step)
-
 
 data_step.to_csv('data_step.csv', index=True, header=True)
 print('finished')
